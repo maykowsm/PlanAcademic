2d7221160bca6adb6118ec49f15c7bf8ebe6374c
Maykow Menezes <eng.maykowmenezes@gmail.com>
2024-02-07 22:38:13 -0300

    implementa ligação entre cards com linhas e colunas, e ordenação pseudoaleatória no load

--- js/svg.js
+++ js/svg.js
@@ -3,6 +3,9 @@ var functionPreload //função para animação inicial em loop
 var rectPreLoad = [] //lista de retangulos para animação inicial
 var cards = [] //lista de cards de materias
 var listDependencias = [] //lista de materias interdependentes
+var listParCardDependente = [] // lista os pares dard, dependencia
+var listLinhasPontos = [] //linhas e pontos de conecção entre os cards
+var seed = 1996
 
 // estilos dos cards
 // Normal
@@ -24,6 +27,12 @@ var opacidadeDesfoco = 0.5
 var corCardDependente = new SVG.Color('rgb(55, 158, 184)')
 var corBordaDependente = new SVG.Color('rgb(55, 158, 184)')
 var opacidadeDependente = 0.5
+
+
+//estilos das linhas e pontos
+var corLinhas = new SVG.Color('rgb(255, 255, 255)')
+var espessuraLinha = 3
+var diametroPontos = 5
 
 
 //Cria a animação principal randomizando o numero de retangulos em cada coluna a cada interação
@@ -122,6 +131,35 @@ function getDependencias(card){
     
 }
 
+// lista par [card, dependente]
+function cardDependente(){
+    listParCardDependente = []
+    listDependencias.forEach(card=>{
+        card.data('dependencias').toString().split('-').forEach(dependencia=>{
+            listDependencias.forEach(cardDependencia=>{
+                if(cardDependencia.data('id') == dependencia){
+                    listParCardDependente = listParCardDependente.concat([[card, cardDependencia]])
+                }
+            })
+        })
+    })
+}
+
+// pega os pontos de entrada e saida para referencia das linhas bisel
+function getPoints(card){
+    return([[card.x(), card.y() + (card.height()/2)], [card.x() + card.width(), card.y() + (card.height()/2)]])
+}
+
+//Gerador de numeros pseudos aleatórios
+function pseudoAleatorios(semente){
+    var state = semente
+
+    return function(){
+        var x = Math.sin(state++) * 10000;
+        return Math.abs(x - Math.floor(x))
+    }
+}
+
 // Cria a interface baseado no jsonData
 function load(){
     var recuoSuperior = 20
@@ -130,7 +168,7 @@ function load(){
     var espacamento_x = 10
     var espacamento_y = 10
     var raio = 5
-
+    var random = pseudoAleatorios(seed)
     clearCards()
     let materias = Object.entries(jsonData['materias'])
     var num_colunas = 0
@@ -142,6 +180,7 @@ function load(){
 
     for(let i = 0; i<= num_colunas; i++){
         let materias  = getmaterias('semestre', i+1)
+        materias.sort(function(){ return random()-0.5})
         
         for(let j = 0; j< materias.length; j++){
             card = canva.rect(largura, altura)
@@ -165,7 +204,7 @@ function load(){
     } 
 
 }
-
+// altera a visualização dos card dependentes
 function showDependentes(focoCard){
     // console.log(listDependencias);
     listDependencias.forEach(card=>{
@@ -175,12 +214,44 @@ function showDependentes(focoCard){
     })
 }
 
+function showLines(){
+    if(listDependencias.length > 1){
+        listParCardDependente.forEach(par=>{
+            // ponto inicial e final
+            p1 = getPoints(par[0])[0]
+            p2 = getPoints(par[1])[1]
+            // pontos para configuração da curva correspondentes respectivamente ao ponto 1 e 2
+            d = Math.abs(p1[0]-p2[0]) //distancia horozontal entre os pontos
+            p3 = [p1[0] - (2/3) * d, p1[1]]
+            p4 = [p2[0] + (2/3) * d, p2[1]]
+            caminho = new SVG.PathArray([
+                ['M', p1[0], p1[1]],
+                ['C', p3[0],p3[1], p4[0], p4[1], p2[0], p2[1]]
+            ])
+            curva = canva.path(caminho).attr({fill: 'none' })
+            curva.stroke({ color: corLinhas, width: espessuraLinha})
+            
 
-//Verifica se o mouse inside sobre o card
+            circulo_1 = canva.circle(diametroPontos).move(p1[0]-(diametroPontos/2), p1[1]-(diametroPontos/2)).fill(corLinhas)
+            circulo_2 = canva.circle(diametroPontos).move(p2[0]-(diametroPontos/2), p2[1]-(diametroPontos/2)).fill(corLinhas)
+            listLinhasPontos.push(circulo_1)
+            listLinhasPontos.push(circulo_2)
+            listLinhasPontos.push(curva)
+            
+        })
+    }
+}
+
+
+//Verifica se o mouse inside sobre o card e altera sua visualização
 function insideCard(X, Y){
     cards.forEach(card=>{
         card.attr({fill: corCardDesfoco, stroke: corBordaDesfoco, opacity: opacidadeDesfoco})
     })
+    for(let i = 0; i< listLinhasPontos.length; i++){
+        listLinhasPontos[i].remove()
+    }
+    
     cards.forEach(card=> {
         if(card.inside(X, Y)){
             
@@ -188,16 +259,20 @@ function insideCard(X, Y){
             listDependencias.push(card)
             getDependentes(card)
             getDependencias(card)
+            cardDependente()            
+            showLines()
             
-            showDependentes(card)
+            
+            showDependentes(card) //altera a visualização dos cards dependente
             // console.log(listDependencias);
 
-            card.attr({fill: corCardHover, stroke: corBordaHover, opacity: opacidadeHover})            
+            card.attr({fill: corCardHover, stroke: corBordaHover, opacity: opacidadeHover})
+            card.stroke({width: 2})           
         }
     })
 }
 
-// identifica o movimento do mouse
+// identifica o movimento do mouse e altera a visualização dos card sem foco
 function mouseMove(event){
     console.log('mouseMove');
     insideCard(event.clientX, event.clientY)
@@ -206,11 +281,8 @@ function mouseMove(event){
 
     if(event.clientY > 8 * 50){
         cards.forEach(card=>{
-            // card.animate(10).fill(corCard).stroke(corBorda).opacity(opacidade)
             card.attr({fill: corCard, stroke: corBorda, opacity: opacidade})
-            // card.fill(corCard)
-            // card.stroke(corBorda)
-            // card.opacity(opacidade)
+
         })
     }
 
